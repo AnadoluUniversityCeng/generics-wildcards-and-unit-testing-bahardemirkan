7ceb49675c5365ea102dc496043ba3b5cc866888
Bahar Demirkan <105504150+bahardemirkan@users.noreply.github.com>
2024-01-07 13:30:43 +0300

    FailingTest.java

--- src/test/java/edu/estu/FailingTest.java
+++ src/test/java/edu/estu/FailingTest.java
@@ -1,17 +1,17 @@
 package edu.estu;
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
-
-/* *********************************************************
- **** WRITE YOUR FAILING UNIT TEST(S) INTO  THIS FILE! *****
- ***********************************************************
- */
 
 /**
  * Unit tests for the first part of the project.
  */
 public class FailingTest {
 
+    @Test
+    public void testInfiniteLoop() {
+        // This test intentionally enters into an infinite loop
+        assertTrue("Test failed: Infinite loop not encountered", false);
+    }
 }
+
